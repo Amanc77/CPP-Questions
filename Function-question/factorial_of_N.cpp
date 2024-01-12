1a413c99fd75a53bff5e794d27bbd4bd301edd7c
Aman Kumar Chaurasiya <amanchaurasiya255@gmail.com>
2024-01-13 00:01:53 +0530

    .

--- Function-question/factorial_of_N.cpp
+++ Function-question/factorial_of_N.cpp
@@ -2,12 +2,13 @@
 using namespace std;
 int main(){
     int n,i ,a=1;
+    cout<<"enter the number  ";
     cin>>n;
     for(i=1;i<=n;i++){
 
         a=i*a;
 
-        cout<<a<<endl;
+        cout<<a<<" ";
     }
     return 0;
 }
