e13b7151b2e0b069d05e468fdae0b30bc7bdec6e
Aman Kumar Chaurasiya <amanchaurasiya255@gmail.com>
2024-01-16 00:05:59 +0530

    done more coditional question

---
+++ Conditional_statement/GradOfSubjct.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int ph, ch, bi, mat, com;
+    cout << "enter number in physics" << endl;
+    cin >> ph;
+    cout << "enter number in chemistry" << endl;
+    cin >> ch;
+    cout << "enter number in biology" << endl;
+    cin >> bi;
+    cout << "enter number in math" << endl;
+    cin >> mat;
+    cout << "enter number in computer" << endl;
+    cin >> com;
+
+    int totalNumber = ph + ch + bi + mat + com;
+    float percentage = (totalNumber / 500.0) * 100;
+
+    if (percentage >= 90)
+    {
+        cout << percentage << " by this percentage you will get Grade A" << endl;
+    }
+    else if (percentage >= 80)
+    {
+        cout << percentage << " by this percentage you will get Grade B" << endl;
+    }
+    else if (percentage >= 70)
+    {
+        cout << percentage << " by this percentage you will get Grade C" << endl;
+    }
+    else if (percentage >= 60)
+    {
+        cout << percentage << " by this percentage you will get Grade D" << endl;
+    }
+    else if (percentage >= 40)
+    {
+        cout << percentage << " by this percentage you will get Grade E" << endl;
+    }
+    else
+    {
+        cout << percentage << " by this percentage you will get Grade F, and you have failed. Please give the exam again." << endl;
+    }
+
+    return 0;
+}
