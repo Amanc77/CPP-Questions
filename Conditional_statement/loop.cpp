aa533547d472d225dab31cbc05d972900f519c0f
Aman Kumar Chaurasiya <amanchaurasiya255@gmail.com>
2024-02-23 08:56:36 +0530

    odd  and even

--- Conditional_statement/loop.cpp
+++ Conditional_statement/loop.cpp
@@ -3,19 +3,32 @@ using namespace std;
 int main ()
 {
     int n , i;
-    cout<<"Enter the number"<<endl;
+    cout<<"Enter the number from where you want odd and even number : ";
     cin>>n;
 
-        cout<<"the number are"<<endl;
+        cout<<"odd  number are : ";
+
     for(i=1;i<=n; i++)
     {
         if(i%2!=0)
-        cout<<i<<endl;
+        cout<<i<<" , ";
 
     
 
 
-}
+    }
+cout<<endl;
+    cout<<"even number are : ";
+    for(i=1;i<=n; i++)
+    {
+        if(i%2!=0)
+        cout<<i+1<<" , ";
+
+    
+
+
+    }
+    cout<<endl;
     return 0;
 
 }
